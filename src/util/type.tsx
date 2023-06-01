ef2bda12ee2403f6660ee9ca5b2b6a028fc6dc56
jasmine0714 <woooooooow22@gmail.com>
2023-06-01 16:39:41 +0900

    🚚 Discussion type 파일로 이동

--- src/util/type.tsx
+++ src/util/type.tsx
@@ -1,6 +1,32 @@
+export type Discussion = {
+  answer: {
+    author: string;
+    avatarURL: string;
+    bodyHTML: string;
+    createdAt: string;
+    id: number;
+    url: string;
+  } | null;
+  author: string;
+  avatarURL: string | null;
+  bodyHTML: string | null;
+  createdAt: string;
+  id: number;
+  url: string | null;
+  title: string;
+  updatedAt: string | null;
+};
+
 export type ItemsPerPage = 4 | 5;
 
 export interface IPageBtns {
   totalPages: number;
   onPageChange: (buttonNumber: number) => void;
 }
+
+export type ToastAction = "ADD" | "DELETE" | "UPDATE";
+
+export type ToastMsg =
+  | "질문이 추가되었습니다."
+  | "질문이 수정되었습니다."
+  | "질문이 삭제되었습니다.";
